3c8e2d3e367fe2d5080e31d352a128dad30e41f6
Piyush Barve <barvepiyush1000@gmail.com>
2020-10-22 14:26:19 +0530

    code upload success

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+import MyName from "./myName";
+import AddAdmin from './form';
+class App extends Component {
+    state={
+     allNames: [
+      {name:'Piyush Barve', age:21 , work:'Student', id:1},
+      {name:'Yoshi Master', age:31, work:'Designer',id:2},
+      {name:'Ryu Ninja', age:26 , work:'Developer',id:3},
+      {name:'Mark Maner', age:21 , work:'Content-Writer',id:4}
+      ]
+    }
+    addfunc = (admin)=>{
+      admin.id = Math.random()
+      let allNames = [...this.state.allNames,admin]
+      this.setState({
+        allNames: allNames
+      })
+      // console.log(this.state)
+    }
 
-function App() {
+    deleteAdmin = (id)=>{
+      let allNamesNew = this.state.allNames.filter(name=>{
+        return name.id !== id
+      })
+
+      this.setState({
+        allNames: allNamesNew
+      }
+      )
+    }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <h1 className='title is-size-2 pt-5 has-text-success'>All Admins</h1>
+        <AddAdmin addfunc = {this.addfunc}/>
+        
+        <MyName  allNames = {this.state.allNames} deleteAdmin = {this.deleteAdmin} />
+        
+        
     </div>
   );
+  }
 }
 
 export default App;
